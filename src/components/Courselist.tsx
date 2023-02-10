2091a58b889b0ef1dadc44b0796c544480c1a888
Haroon <walkin.logic@gmail.com>
2023-02-10 17:42:35 +0500

    #update course list design

--- src/components/Courselist.tsx
+++ src/components/Courselist.tsx
@@ -19,21 +19,35 @@ const Courselist = ({ text, short, image }: props) => {
             <Center py={6}>
                 <Box
                     maxW={'445px'}
+                    maxH={'410px'}
                     w={'full'}
+                    mb={6}
                     bg={useColorModeValue('white', 'gray.900')}
                     boxShadow={'2xl'}
                     rounded={'md'}
                     p={6}
                     overflow={'hidden'}>
-                    <Stack>
-                        <Image src={image} />
+                    <Box
+                        maxH={'210px'}
+                        bg={'gray.100'}
+                        mt={-6}
+                        mx={-6}
+                        mb={6}
+                        overflow={'hidden'}
+                        pos={'relative'}>
+                        <Image
+                            src={image}
+                            layout={'fill'}
+                        />
+                    </Box>
+                    <Stack mb={10} h={'200px'}>
                         <Heading
                             color={useColorModeValue('gray.700', 'white')}
-                            fontSize={'2xl'}
+                            fontSize={'1xl'}
                             fontFamily={'body'}>
                             {text}
                         </Heading>
-                        <Text color={'gray.500'}>
+                        <Text color={'gray.500'} noOfLines={[3, 4, 5]}>
                             {short}
                         </Text>
                     </Stack>
