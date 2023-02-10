88226a161249b597c613a477ba98dc837d0995a8
Haroon <walkin.logic@gmail.com>
2023-02-10 17:15:21 +0500

    #update images

--- src/pages/howitworks.tsx
+++ src/pages/howitworks.tsx
@@ -89,7 +89,7 @@ const HowitworksPage: React.FC = () => {
                             rounded={'md'}
                             alt={'feature image'}
                             src={
-                                'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                                '/static/images/splash.jpg'
                             }
                             objectFit={'cover'}
                         />
