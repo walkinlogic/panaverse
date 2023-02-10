7eb0b7583214eb2f5ab876e0e53076a766e586b6
Haroon <walkin.logic@gmail.com>
2023-02-10 17:18:29 +0500

    #update course images

--- src/pages/courses.tsx
+++ src/pages/courses.tsx
@@ -94,7 +94,7 @@ const CoursesPage: React.FC = () => {
                             rounded={'md'}
                             alt={'feature image'}
                             src={
-                                'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+                                '/static/images/courses.jpg'
                             }
                             objectFit={'cover'}
                         />
